0f7c25e2eb8b7be28c6e584001f63f757c30758a
Soohyeuk <csh040816@gmail.com>
2025-03-25 00:28:41 -0400

    all things work

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig({
       external: ['react', 'react-dom'],
       output: {
         manualChunks: {
-          react: ['react', 'react-dom'],
           timeline: ['react-vertical-timeline-component']
         },
         globals: {
