d0e27b253c1b4093cd557b2a91a888294895d81f
Soohyeuk <csh040816@gmail.com>
2025-03-24 23:59:04 -0400

    trying different things for deployment

--- portfolio/components/Landing.tsx
+++ portfolio/components/Landing.tsx
@@ -1,7 +1,12 @@
+import headshot from '../assets/head.jpg'
+import pdfIcon from '../assets/pdf.svg'
+import linkedinIcon from '../assets/linkedin-logo.svg'
+import githubIcon from '../assets/github-logo.svg'
+import resume from '../assets/Resume_Soohyeuk_Choi.pdf'
 const Landing = () => {
   const handleDownloadResume = () => {
     const link = document.createElement('a');
-    link.href = './assets/Resume_Soohyeuk_Choi.pdf'; 
+    link.href = resume; 
     link.download = 'Resume_Soohyeuk_Choi.pdf'; 
     document.body.appendChild(link);
     link.click();
@@ -12,7 +17,7 @@ const Landing = () => {
     <div className="flex flex-col items-center justify-center max-w-[50rem] mx-auto px-4 -mt-10">
       <div className='flex flex-row items-center justify-center gap-10'>
         <div className="relative w-40 h-40 sm:w-44 sm:h-44 md:w-52 md:h-52 lg:w-56 lg:h-56 rounded-full p-[0.3rem] bg-gradient-to-r from-teal-400 via-cyan-500 to-indigo-500 dark:from-indigo-500 dark:via-purple-500 dark:to-teal-400 shadow-xl">
-          <img src={"../assets/head.jpg"} alt="profile" className="rounded-full object-cover w-full h-full" />
+          <img src={headshot} alt="profile" className="rounded-full object-cover w-full h-full" />
         </div>
       </div>
       <div className='mt-5'>
@@ -28,7 +33,7 @@ const Landing = () => {
               onClick={handleDownloadResume}
               className='bg-gradient-to-r from-teal-400 via-cyan-500 to-indigo-500 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 text-white px-4 sm:px-6 py-3 rounded-full transition-all duration-300 flex items-center gap-2 hover:scale-105'
             >
-              <img src={"../assets/pdf.svg"} alt="resume" className='w-5 h-5 sm:w-6 sm:h-6' />
+              <img src={pdfIcon} alt="resume" className='w-5 h-5 sm:w-6 sm:h-6' />
               <span className="sm:hidden">Resume</span>
               <span className="hidden sm:inline">Download Resume</span>
             </button>
@@ -38,7 +43,7 @@ const Landing = () => {
               className='bg-gradient-to-r from-teal-400 via-cyan-500 to-indigo-500 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 text-white p-3 sm:px-6 sm:py-3 rounded-full transition-all duration-300 flex items-center gap-2 hover:scale-105'
               aria-label="LinkedIn Profile"
             >
-              <img src={"../assets/linkedin-logo.svg"} alt="linkedin" className='w-5 h-5 sm:w-6 sm:h-6' />
+              <img src={linkedinIcon} alt="linkedin" className='w-5 h-5 sm:w-6 sm:h-6' />
               <span className='hidden sm:inline'>LinkedIn</span>
             </button>
 
@@ -47,7 +52,7 @@ const Landing = () => {
               className='bg-gradient-to-r from-teal-400 via-cyan-500 to-indigo-500 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 text-white p-3 sm:px-6 sm:py-3 rounded-full transition-all duration-300 flex items-center gap-2 hover:scale-105'
               aria-label="GitHub Profile"
             >
-              <img src={"../assets/github-logo.svg"} alt="github" className='w-5 h-5 sm:w-6 sm:h-6' />
+              <img src={githubIcon} alt="github" className='w-5 h-5 sm:w-6 sm:h-6' />
               <span className='hidden sm:inline'>GitHub</span>
             </button>
         </div>
