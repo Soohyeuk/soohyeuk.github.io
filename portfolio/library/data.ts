4d344dffe27ce2520e931a1c99ea1b09284c3d0a
Soohyeuk <csh040816@gmail.com>
2025-04-04 18:33:29 -0400

    unused import error fixed

--- portfolio/library/data.ts
+++ portfolio/library/data.ts
@@ -1,7 +1,6 @@
 import proj1 from "../assets/proj1.jpg";
 import proj2 from "../assets/proj2.jpg";
 import proj3 from "../assets/proj3.jpg";
-import wip from "../assets/wip.jpg";
 import proj4 from "../assets/proj4.jpg";
 
 // Import all skill icons
