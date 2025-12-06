a14981d5c12da9008197ea0a1e5191296f4008b7
Soohyeuk <csh040816@gmail.com>
2025-12-06 01:05:39 -0500

    end date of civic updated

--- portfolio/library/data.ts
+++ portfolio/library/data.ts
@@ -108,7 +108,7 @@ export const experiencesData: Experience[] = [
         company: "Civic",
         position: "Software Engineer Intern",
         startDate: "May. 2025",
-        endDate: "Jul. 2025",
+        endDate: "August. 2025",
         description: "Launched template features within the embedded Rich Text Editor to streamline email automation, improving efficiency by 40%; utilized TypeScript, PlateJS, and tRPC server components. Designing and implementing data migration of over 20+ databases deployed in Azure to be consolidated under PostgreSQL using Prisma. Outlined internal tools to incorporate user feedback into ML model predictions for email automation, resulting in a 100% satisfaction rate from coworkers and a 10% improvement in model accuracy; utilized Matplotlib, NumPy, PostgreSQL, Next.js (TypeScript).",
         tags: [
             { icon: nextjsIcon, name: "Next.js" },
